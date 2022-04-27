834da562e0f271d9bea9e1cd029aee4ede9f952f
Edward E Greenwood <edwardgreenwood38@gmail.com>
2022-04-26 22:04:39 -0400

    basic form done
    layout completed and basic functions done.

---
+++ XamarimApp2/XamarimApp2/XamarimApp2/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarimApp2
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+            
+            EraseNoteCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveNoteCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+
+                TheNote = string.Empty;
+            });
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveNoteCommand { get; }
+
+        public Command EraseNoteCommand { get; }
+    }
+}
